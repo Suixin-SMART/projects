8db3699b98dd3483f4caef83f3d465b405c8a034
Nomce <naum.spaseski@gmail.com>
2015-11-05 23:47:03 +0100

    lelann forme

--- timetable/modelisation.pl
+++ timetable/modelisation.pl
@@ -1,6 +1,6 @@
-:- use_module(library(clpq)).
+:- use_module(library(clpfd)).
 
-%(salle, capacité, liste des dates libres[jour, debut, duree])
+%(+salle, -capacité, -liste des dates libres[jour, debut, duree])
 salle('Amphi A', 100,
   [[1, 8, 180], [1, 28, 120],
   [2, 16, 120],
@@ -13,16 +13,22 @@ salle('Amphi C', 50,
   [3, 28, 120], [3, 44, 120],
   [5, 28, 120], [5, 44, 120]]).
 
-%(nom, taille du groupe)
+%(+nom, -taille du groupe)
 groupe('Master 1 Info', 56).
+groupe('Master 2 Info CODES', 10)
+groupe('Master 2 Info SDR', 15)
 
-%(nom, duree, [promotions])
-epreuve('identifiant','GRAF', 120, ['Master 1 Info']).
+%(+nom, -duree, -[promotions])
+epreuve('GRAF', 120, ['Master 1 Info']).
 epreuve('GL', 90, ['Master 1 Info']).
 epreuve('COMP', 180, ['Master 1 Info']).
 epreuve('RESO', 90, ['Master 1 Info']).
 epreuve('SAMP', 240, ['Master 1 Info']).
-epreuve('AD', 240, 28, ['Master 2 Info SECO', 'Master 2 Info SDR']).
+epreuve('AD', 240, ['Master 2 Info CODES','Master 2 Info SDR']).
+epreuve('Test', 240, ['Master 2 Info CODES','Master 2 Info SDR']).
+epreuve('OC', 240, ['Master 2 Info CODES','Master 2 Info SDR']).
+epreuve('IDM', 240, ['Master 2 Info CODES']).
+epreuve('PAD', 240, ['Master 2 Info SDR']).
 
 %reservation(IdEpreuve, IdSalle, [[Jour, Heure, DureeLibre] | SalleHoraires], NEW_SALLE, Reservation):-
 %  EPREUVE = SALLE.
@@ -40,6 +46,44 @@ epreuve('AD', 240, 28, ['Master 2 Info SECO', 'Master 2 Info SDR']).
 % DeltaMinEntreExamens = entier (durée en minutes par exemple)
 % Resultat =
 
+nbEtudiants([],0).
+
+nbEtudiants([Groupe|ResteGroupe],Rez):-
+  groupe(Groupe,nb),
+  nbEtudiants(ResteGroupe,NewRez),
+  Rez = nb + NewRez.
+
+getDispoDuree([Jour, Debut, Duree], RezDuree):-
+  RezDuree = Duree.
+
+getHoraire(Duree, [], 0, []):-
+
+getHoraire(Duree, [FirstDispo|ResteDispo], Rez, NewDispo):-
+  getDispoDuree(FirstDispo, Dispo),
+  Dispo < Duree,
+  getHoraire(Duree, ResteDispo, Rez, NewDispo).
+
+getHoraire(Duree, [FirstDispo|ResteDispo], Rez, NewDispo):-
+  getDispoDuree(FirstDispo, Dispo),
+  Dispo >= Duree,
+  Rez = FirstDispo,
+  NewDispo = ResteDispo.
+
+
+comparer(Examen, Amphi, Rez):-
+  epreuve(Examen, EpreuveMinutes, ListeEtudiants),
+  nbEtudiants(ListeEtudiants, NbEtudiants),
+  salle(Amphi, Capacite, ListeDisponibles),
+  NbEtudiants >= Capacite,
+  getHoraire(EpreuveMinutes, ListeDisponibles, Horaire, NewListeDisponibles),
+  ListeDisponibles = NewListeDisponibles
+
+%(+listeEpreuves, +listeSalles, -listeResultats)
+schedule([Examen|ResteExamen],[Amphi|ResteAmphis], Resultat):-
+  comparer(Examen, Amphi, Rez).
+
+
+
 %addContraints(Param, S0, S):-
 %  gen_state(S0),
 %  constraint().
@@ -49,12 +93,13 @@ epreuve('AD', 240, 28, ['Master 2 Info SECO', 'Master 2 Info SDR']).
 %  length(PropositionAffectation, NbAffectations),
 %  minimize(NbAffectations, State0, StateNew).
 
-  knapsack_constrain(S) :-
-        gen_state(S0),
-        constraint([6*x(1), 4*x(2)] =< 8, S0, S1),
-        constraint([x(1)] =< 1, S1, S2),
-        constraint([x(2)] =< 2, S2, S).
-
-knapsack(S) :-
-        knapsack_constrain(S0),
-        maximize([7*x(1), 4*x(2)], S0, S).
+%  knapsack_constrain(S) :-
+%        gen_state(S0),
+%        constraint([6*x(1), 4*x(2)] =< 8, S0, S1),
+%        constraint([x(1)] =< 1, S1, S2),
+%        constraint([x(2)] =< 2, S2, S).
+%
+%knapsack(S) :-
+%        knapsack_constrain(S0),
+%        maximize([7*x(1), 4*x(2)], S0, S).
+%
